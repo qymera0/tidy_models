077b98e93f47e7f5da3fca935594a57e42aa6e26
Samuel B Baco <samuelbaco@gmail.com>
2020-09-14 15:01:33 -0300

    sMOTE-ROSE

--- 03_create_robust_models/Subsampling for class imabalances.R
+++ 03_create_robust_models/Subsampling for class imabalances.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(tidymodels)
+library(themis)
+library(discrim)
 
 # SIMULATED DATA ----------------------------------------------------------
 
@@ -9,13 +13,74 @@ dim(imbal_data)
 
 table(imbal_data$Class)
 
-
 # SUBSAMPLING DATA --------------------------------------------------------
-
-library(tidymodels)
-
-library(themis)
 
 imbal_rec <-
   recipe(Class ~., data = imbal_data) %>% 
-  step_rose(class)
+  step_rose(Class)
+
+qda_mod <-
+  discrim_regularized(frac_common_cov = 0, frac_identity = 0) %>% 
+  set_engine("klaR")
+
+qda_rose_wflw <-
+  workflow() %>% 
+  add_model(qda_mod) %>% 
+  add_recipe(imbal_rec)
+
+qda_rose_wflw
+
+# MODEL PERFORMANCE -------------------------------------------------------
+
+set.seed(5732)
+
+# Uses a recipie with balanced data to generate the folds.
+
+cv_folds <-
+  vfold_cv(imbal_data, strata = "Class", repeats = 5)
+
+cls_metrics <- metric_set(roc_auc, j_index)
+
+set.seed(2180)
+
+qda_rose_res <- 
+  fit_resamples(
+    qda_rose_wflw, 
+    resamples = cv_folds, 
+    metrics = cls_metrics
+)
+
+collect_metrics(qda_rose_res)
+
+# Without Sampling
+
+qda_wflw <- 
+  workflow() %>% 
+  add_model(qda_mod) %>% 
+  add_formula(Class ~ .)
+
+set.seed(2180)
+
+qda_only_res <- fit_resamples(qda_wflw, resamples = cv_folds, metrics = cls_metrics)
+
+collect_metrics(qda_only_res)
+
+# Plot Metrics
+
+no_sampling <- 
+  qda_only_res %>% 
+  collect_metrics(summarize = FALSE) %>% 
+  dplyr::select(-.estimator) %>% 
+  mutate(sampling = "no_sampling")
+
+with_sampling <- 
+  qda_rose_res %>% 
+  collect_metrics(summarize = FALSE) %>% 
+  dplyr::select(-.estimator) %>% 
+  mutate(sampling = "rose")
+
+bind_rows(no_sampling, with_sampling) %>% 
+  mutate(label = paste(id2, id)) %>%  
+  ggplot(aes(x = sampling, y = .estimate, group = label)) + 
+  geom_line(alpha = .4) + 
+  facet_wrap(~ .metric, scales = "free_y")
